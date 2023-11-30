1d6d8bd926b484e9cf82298fdda8538eaafce70b
Helene <helene.quintard@lapiscine.pro>
2023-11-30 11:38:45 +0100

    START DAY 3 -- add explanations on assignement

--- app.js
+++ app.js
@@ -25,8 +25,11 @@ app.get('/api/coworkings/:id', (req, res) => {
   res.json(result)
 })
 
-app.post('/api/coworkings', (req, res) => {
+// implémenter le endpoint post qui renvoie une réposne "post fonctionne"
+app.post('/api/coworkings/', (req, res) => {
+  // Ajouter le coworking dans le tableau, en automatisant la génération d'un id. On récupère le dernier élément du tableau et on ajoute +1 à son id.
   // let coworking = req.body
+  
   const newId = mockCoworkings[mockCoworkings.length - 1].id + 1
   // let coworking = {superficy : req.body.superficy, capacity : req.body.capacity, name: req.body.name}
 
@@ -34,16 +37,26 @@ app.post('/api/coworkings', (req, res) => {
   let coworking = {id: newId,...req.body}
   mockCoworkings.push(coworking)
 
+  //On renvoie un objet qui contient les proriétés message et data
+  // message: `Le coworking a bien été ajouté`
  const result ={message :'Le coworking a bien été ajouté', data : coworking} 
   res.json(result)
 })
 
+// implémenter le endpoint put coworkings avec :id, ainsi que la requête correspondante dans Postman
 app.put('/api/coworkings/:id',(req,res)=>{
-  const coworking = mockCoworkings.find(el => el.id === parseInt(req.params.id))
+  // la méthode find renvoie un nouvel objet, clone de celui contenu dans le tableau
+  let coworking = mockCoworkings.find(el => el.id === parseInt(req.params.id))
   
   let result;
   if (coworking) {
-    coworking.superficy = req.body.superficy
+    // coworking.superficy = req.body.superficy
+
+    //DISTINCTION IMPORTANTE entre l'id des objets contenus dans le tableau,
+    //et l'index de ces objets dans le tableau, par exemple: Oasis Coworking a pour .id 12 et pour index dans le tableau [2]
+    const newCoworking = {...coworking, ...req.body}
+    const index = mockCoworkings.findIndex(el=> el.id === parseInt(req.params.id))
+    mockCoworkings[index] = newCoworking
     result ={message :'coworking modifié', data : coworking}
   } else {
     result  = {message :`Le coworking n'existe pas`, data: {}}
