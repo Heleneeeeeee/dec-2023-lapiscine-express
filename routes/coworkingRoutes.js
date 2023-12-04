552ca113ca7e6ca21b06e4a5c857d3f5b0268758
Helene <helene.quintard@lapiscine.pro>
2023-12-04 14:04:13 +0100

    Complete Crud for coworkings in db

--- routes/coworkingRoutes.js
+++ routes/coworkingRoutes.js
@@ -1,115 +1,77 @@
 const express = require('express')
 const router = express.Router()
+// const { Op } = require('sequelize')
 const { Coworking } = require('../db/sequelizeSetup')
-let mockCoworkings = require('../mock-coworking')
-const coworkings = require('../models/coworkings')
 
 router
     .route('/')
     .get((req, res) => {
-        Coworking.findAll ()
-            .then((coworkings) => {
-                res.json(coworkings)
-                console.log(coworkings)
+        console.log(req.query);
+        Coworking.findAll()
+            .then((results) => {
+                res.json(results)
             })
-            . catch ((error) => {
+            .catch(error => {
                 res.json(error.message)
-            })    
+            })
     })
-
     .post((req, res) => {
-        Coworking.update
-        // const newId = mockCoworkings[mockCoworkings.length - 1].id + 1
-        // let coworking = { id: newId, ...req.body }
-        // mockCoworkings.push(coworking)
-            const newCoworking = {...req.body}
-            
-            // Coworking.create(newCoworking)
-            
-        // Coworking.create({
-        //     name: "Oasis Coworking",
-        //     price: { "hour": 4, "day": 21, "month": 100 },
-        //     address: { "number": "68bis", "street": "avenue Jean Jaurès", "postCode": 33150, "city": "Cenon" },
-        //     superficy: 200,
-        //     capacity: 27,
-        // })
-        Coworking.create (newCoworking)
+        const newCoworking = { ...req.body }
+
+        Coworking.create(newCoworking)
             .then((coworking) => {
-                res.json({message: 'Le coworking a bien été créé', data:coworking})
+                res.json({ message: 'Le coworking a bien été créé', data: coworking })
                 console.log(coworking)
             })
-            . catch ((error) => {
-                res.json({message: `Le coworking n'a pu être créé`, data:error.message})
-            })    
-
-        // const result = { message: `Le coworking a bien été ajouté`, data: newCoworking }
-       
+            .catch((error) => {
+                res.json({ message: `Le coworking n'a pas pu être créé`, data: error.message })
+            })
     })
 
 router
     .route('/:id')
     .get((req, res) => {
-        let result = mockCoworkings.find(el => el.id === parseInt(req.params.id))
-
-        if (!result) {
-            result = `Aucun élément ne correspond à l'id n°${req.params.id}`
-        }
-        res.json(result)
+        Coworking.findByPk(parseInt(req.params.id))
+            .then((result) => {
+                console.log(result)
+                if (result) {
+                    res.json({ message: 'Un coworking a été trouvé.', data: result })
+                } else {
+                    res.json({ message: `Aucun coworking n'a été trouvé.` })
+                }
+            })
+            .catch((error) => {
+                res.json({ message: 'Une erreur est survenue.', data: error.message })
+            })
     })
 
     .put((req, res) => {
-    Coworking.update({ name: "WIN Mériadeck" }, {
-        where: {
-            name: "WIN"
-        }
-    })
-    .then((coworking) => {
-        res.json({ message: 'Le coworking a bien été modifié', data: coworking });
-        console.log(coworking);
-    })
-    .catch((error) => {
-        res.json({ message: `Le coworking n'a pas pu être modifié`, data: error.message });
-    });
-})
-        // let coworking = mockCoworkings.find((el) => el.id === parseInt(req.params.id))
-
-        // let result;
-        // if (coworking) {
-        //     const newCoworking = { ...coworking, ...req.body }
-        //     const index = mockCoworkings.findIndex(el => el.id === parseInt(req.params.id))
-        //     mockCoworkings[index] = newCoworking
-        //     result = { message: 'Coworking modifié', data: newCoworking }
-        // } else {
-        //     result = { message: `Le coworking n'existe pas`, data: {} }
-        // }
-
-        // res.json(result)
-
-
-    .delete((req, res) => {
-        Coworking.destroy({
+        Coworking.update(req.body, {
             where: {
-                name: "Oasis Coworking"
+                id: req.params.id
             }
         })
-        .then((coworking) => {
-            res.json({ message: 'Le coworking a bien été supprimé', data: coworking });
-            console.log(coworking);
-        })
-        .catch((error) => {
-            res.json({ message: `Le coworking n'a pas pu être supprimé`, data: error.message });
-        });
-    });
-        // const coworking = mockCoworkings.find((el) => el.id === parseInt(req.params.id))
+            .then((result) => {
+                if (result > 0) {
+                    res.json({ message: 'Le coworking a bien été mis à jour.', data: result })
+                } else {
+                    res.json({ message: `Aucun coworking n'a été mis à jour.` })
+                }
+            })
+            .catch(error => {
+                res.json({ message: 'La mise à jour a échoué.', data: error.message })
+            })
+    }) 
 
-        // let result;
-        // if (coworking) {
-        //     mockCoworkings = mockCoworkings.filter(el => el.id !== coworking.id)
-        //     result = { message: 'Coworking supprimé', data: coworking }
-        // } else {
-        //     result = { message: `Le coworking n'existe pas`, data: {} }
-        // }
-
-        // res.json(result)
+    .delete((req, res) => {
+        Coworking.destroy({ where: { id: req.params.id } })
+            .then((result) => {
+                res.json({ mesage: `Le coworking a bien été supprimé.`, data: result })
+            })
+            .catch((error) => {
+                res.json({ mesage: `La suppression a échoué.`, data: error.message })
+            })
+    })
+       
     
 module.exports = router
